0ae5d880eb62373aeadee5210de522037691ebe3
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-12-04 18:10:29 +0900

    feat: 4번 문제

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/component/BigButton.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/component/BigButton.kt
@@ -1,7 +1,10 @@
 package com.survivalcoding.gangnam2kiandroidstudy.presentation.component
 
+import android.R.attr.text
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -15,6 +18,9 @@ import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,16 +34,24 @@ fun BigButton(
     modifier: Modifier = Modifier,
     onClick: () -> Unit = {},
 ) {
+    val interactionSource = remember { MutableInteractionSource() }
+
+    val isPressed by interactionSource.collectIsPressedAsState()
+
+    val boxColor = if (isPressed) AppColors.gray4 else AppColors.primary100
+
     Box(
         modifier = modifier
             .size(width = 315.dp, height = 60.dp)
             .background(
-                color = AppColors.primary100,
+                color = boxColor,
                 shape = RoundedCornerShape(10.dp)
             )
-            .clickable {
-                onClick()
-            }
+            .clickable(
+                interactionSource = interactionSource,
+                indication = null,
+                onClick = onClick
+            )
     ) {
         Row(
             modifier = Modifier.fillMaxSize(),
@@ -67,5 +81,10 @@ fun BigButton(
 @Preview(showBackground = true)
 @Composable
 fun BigButtonPreview() {
-    BigButton("Button")
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        BigButton("Button")
+    }
 }
--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/component/SmallButton.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/component/SmallButton.kt
@@ -2,6 +2,8 @@ package com.survivalcoding.gangnam2kiandroidstudy.presentation.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,6 +15,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -26,16 +30,24 @@ fun SmallButton(
     modifier: Modifier = Modifier,
     onClick: () -> Unit = {},
 ) {
+    val interactionSource = remember { MutableInteractionSource() }
+
+    val isPressed by interactionSource.collectIsPressedAsState()
+
+    val boxColor = if (isPressed) AppColors.gray4 else AppColors.primary100
+
     Box(
         modifier = modifier
             .size(width = 174.dp, height = 37.dp)
             .background(
-                color = AppColors.primary100,
+                color = boxColor,
                 shape = RoundedCornerShape(10.dp)
             )
-            .clickable {
-                onClick()
-            }
+            .clickable(
+                interactionSource = interactionSource,
+                indication = null,
+                onClick = onClick
+            )
     ) {
         Row(
             modifier = Modifier.fillMaxSize(),
