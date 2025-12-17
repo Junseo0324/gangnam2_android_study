24f168dd8c6c985d4a7d18f3d2675b41a62c7acc
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-12-17 15:44:19 +0900

    feat: 코드 컨벤션

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/ingredient/IngridentScreenRoot.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/ingredient/IngridentScreenRoot.kt
@@ -1,13 +1,7 @@
 package com.survivalcoding.gangnam2kiandroidstudy.presentation.ingredient
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.platform.LocalContext
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewmodel.MutableCreationExtras
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.survivalcoding.gangnam2kiandroidstudy.AppApplication
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
